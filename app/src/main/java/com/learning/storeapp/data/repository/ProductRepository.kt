c3c4e2cd0835b9426a491cf0ce61b0349ec8b657
kaleb.silva <kalebdias.oficial@gmail.com>
2023-08-11 13:51:28 -0400

    feat: create layout list and item to list

--- app/src/main/java/com/learning/storeapp/data/repository/ProductRepository.kt
+++ app/src/main/java/com/learning/storeapp/data/repository/ProductRepository.kt
@@ -6,21 +6,7 @@ import com.learning.storeapp.data.repository.datasource.ProductRemoteDataSource
 class ProductRepository(private val remoteDataSource: ProductRemoteDataSource) {
     suspend fun fetch(): List<ProductApiModel> {
         try {
-            return listOf(
-                ProductApiModel(1, "fdfd"),
-                ProductApiModel(2, "fdfd"),
-                ProductApiModel(3, "fdfd"),
-                ProductApiModel(4, "fdfd"),
-                ProductApiModel(5, "fdfd"),
-                ProductApiModel(6, "fdfd"),
-                ProductApiModel(7, "fdfd"),
-                ProductApiModel(8, "fdfd"),
-                ProductApiModel(9, "fdfd"),
-                ProductApiModel(10, "fdfd"),
-                ProductApiModel(11, "fdfd"),
-                ProductApiModel(12, "fdfd"),
-                ProductApiModel(13, "fdfd"),
-                )
+            return remoteDataSource.fetchProducts()
         } catch (ex: Exception) {
             throw ex
         }
